6b77227159b5784863683f41a5d49f76520cf8f8
cmarker <cmarker4@gmail.com>
2020-10-21 23:37:10 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(array)
+    if array == []
+        puts "The line is currently empty."
+    else
+        line = "The line is currently:"
+
+        array.each_with_index do |name, index|
+            line += " #{index + 1}. #{name}"
+        end
+        puts line
+    end
+end
+
+def take_a_number(array, name)
+    if array.size == 0
+        array << name
+        puts "Welcome, #{name}. You are number 1 in line."
+    end
+end

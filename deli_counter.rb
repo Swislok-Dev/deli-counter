b4c1dd4088f1d0c15fd8728b30b1c5c45c5300ee
cmarker <cmarker4@gmail.com>
2020-10-27 08:00:54 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,19 @@ end
 def take_a_number(array, name)
     if array.size == 0
         array << name
-        puts "Welcome, #{name}. You are number 1 in line."
+        puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+    else
+    
+        array << name
+        puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+
+    end
+end
+
+def now_serving(array)
+    if array.empty? == true
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift}."
     end
 end
